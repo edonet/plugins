ea1b38e731848c37ce9c9c42722514783b0c310c
edonet <edonet@163.com>
2016-05-28 17:07:35 +0800

    更新【webpack-module-exports】

--- webpack-module-exports/index.js
+++ webpack-module-exports/index.js
@@ -1,27 +1,11 @@
 'use strict';
 
-var fs = require('fs'),
-    find = 'return __webpack_require__(0);',
-    replace = `
-/******/    var output = __webpack_require__(0),
-/******/        key;
-
-/******/    if(typeof module !== 'undefined' && typeof exports === 'object'){
-/******/        module.exports = output;
-/******/    }else if(typeof define === 'function' && (define.amd || define.cmd)){
-/******/        define(function(require, exports, module){
-/******/            return module.exports = output;
-/******/        });
-/******/    }else{
-/******/        for(key in output){
-/******/            window[key] = output[key];
-/******/        }
-/******/    }`;
-
-
-function ExportWebpackPlugin(options) {}
+function ExportWebpackPlugin(name) {
+    this.name = name;
+}
 
 ExportWebpackPlugin.prototype.apply = function(compiler, callback) {
+    var name = this.name;
 
     compiler.plugin("compilation", function(compilation) {
         compilation.plugin("optimize-chunk-assets", function(chunks, callback) {
@@ -30,7 +14,7 @@ ExportWebpackPlugin.prototype.apply = function(compiler, callback) {
                     var asset = compilation.assets[file],
                         code = asset.source();
 
-                    replaceWebpackAssetCode(asset, code);
+                    replaceWebpackAssetCode(asset, code, name);
                 });
             });
             callback();
@@ -38,8 +22,43 @@ ExportWebpackPlugin.prototype.apply = function(compiler, callback) {
     });
 };
 
-function replaceWebpackAssetCode(asset, code){
+function replaceWebpackAssetCode(asset, code, name){
+    var find = 'return __webpack_require__(0);',
+        replace = getExportsReplaceContent(name);
+
     asset._cachedSource = code.replace(find, replace);
+}
+
+function getExportsReplaceContent(name){
+    if(typeof name === 'string'){
+        return `
+/******/    var __module_exports__ = __webpack_require__(0);
+
+/******/    if(typeof module !== 'undefined' && typeof exports === 'object'){
+/******/        module.exports = __module_exports__;
+/******/    }else if(typeof define === 'function' && (define.amd || define.cmd)){
+/******/        define(function(require, exports, module){
+/******/            return module.exports = __module_exports__;
+/******/        })
+/******/    }else{
+/******/        window.${name} = __module_exports__;
+/******/    };`;
+    }else{
+        return `
+/******/    var __module_exports__ = __webpack_require__(0);
+
+/******/    if(typeof module !== 'undefined' && typeof exports === 'object'){
+/******/        module.exports = __module_exports__;
+/******/    }else if(typeof define === 'function' && (define.amd || define.cmd)){
+/******/        define(function(require, exports, module){
+/******/            return module.exports = __module_exports__;
+/******/        });
+/******/    }else{
+/******/        for(var key in __module_exports__){
+/******/            window[key] = __module_exports__[key];
+/******/        }
+/******/    }`;
+    }
 }
 
 module.exports = ExportWebpackPlugin;
